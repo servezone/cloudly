1591e3d517327ac56f6e82d908692f4b491cf556
Phil Kunz <github@philkunz.com>
2019-01-13 02:20:58 +0100

    fix(core): update

--- ts/cloudly.classes.ext.mongodb.ts
+++ ts/cloudly.classes.ext.mongodb.ts
@@ -1,10 +1,12 @@
 import * as plugins from './cloudly.plugins';
 
-/**
- * this is the mongodbInstance for cloudly
- */
-export const smartDb = new plugins.smartdata.SmartdataDb({
-  mongoDbName: '',
-  mongoDbPass: '',
-  mongoDbUrl: ''
-});
+export class CloudlyMongodb {
+  /**
+   * this is the mongodbInstance for cloudly
+   */
+  smartDb = new plugins.smartdata.SmartdataDb({
+    mongoDbName: '',
+    mongoDbPass: '',
+    mongoDbUrl: ''
+  });
+}
